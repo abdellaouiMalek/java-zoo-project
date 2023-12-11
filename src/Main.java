e3ad82b25bda7019f4c393cacb27484e1b0e7bfa
Abdellaoui Malek <abdellaoui.maleks@gmail.com>
2023-12-11 18:49:21 +0100

    Prosit 9 : entity Departement

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 import entities.Employe;
+import entities.Departement;
 import entities.SocieteArrayList;
+import entities.DepartementHashSet;
 
 public class Main {
     public static void main(String[] args) {
+        /* -------------Prosit 9---------------------
         SocieteArrayList S = new SocieteArrayList();
         Employe e1 = new Employe(1, "Dridi", "Khalil", "27", 2);
         Employe e2 = new Employe(2, "Seltana", "Malek", "22", 4);
@@ -24,5 +27,18 @@ public class Main {
         System.out.println("************** Tri employé par Depart et grade : *************");
         S.trierEmployeParNomDepartementEtGrade();
         S.displayEmploye();
+    } */
+        // -------------Prosit 10-------------------
+        DepartementHashSet Depart = new DepartementHashSet();
+
+        Depart.ajouterDepartement(new Departement(5, "info", 10));
+        Depart.ajouterDepartement(new Departement(1, "chimie", 2));
+        Depart.ajouterDepartement(new Departement(8, "math", 254));
+        System.out.println("************** Affichage départements : *************");
+        Depart.displayDepartement();
+        System.out.println("************** Recherche département : *************");
+        System.out.println(Depart.rechercherDepartement("physique"));
+        System.out.println("************** Tri département par ID : *************");
+        System.out.println(Depart.trierDepartementById());
     }
 }
